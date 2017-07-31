c4b7b094f29be903b504211df8a3b541a3a74d5e
sujitk0217 <30520313+sujitk0217@users.noreply.github.com>
2017-07-31 15:02:12 +0530

    Sample SQL File
    Sample SQL File

---
+++ Sample Text.sql
@@ -0,0 +1,22 @@
+SELECT  L.LastName
+			,L.FirstName
+			,L.TempID
+			,R.RegionName
+			,L.LastUpdate ImportDate
+			,L.[FileName]
+			,E.LicenseName			
+	FROM MSN_Code.[dbo].[CSSDocumentInsertLog] L
+	LEFT OUTER JOIN MSN_Live.dbo.License_elements E on E.LicenseID = L.LicenseID
+	LEFT OUTER JOIN MSN_LIve.dbo.Profile_Temp T on L.TempID = T.RecordID
+	LEFT OUTER JOIN MSN_Live.dbo.Regions R ON R.RegionID = T.HomeRegion
+	WHERE 1=1
+	AND WasInsertedToCSS = 1
+	AND LastUpdate >= '6/11/2014' -- This is the date the new doc import started.
+	AND LastUpdate BETWEEN @From AND DATEADD(DAY,1,@To)
+	AND T.Certification in (@Certification)
+	--and LicenseID = 2938   -- I9
+	--and LicenseID =  2931 -- E-Verify
+	ORDER BY LastName,FirstName DESC
+
+
+	--Sample changes by Sujit
